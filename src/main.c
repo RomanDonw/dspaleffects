555dc033b110e6dc6fcb87abfb80f23deed13f20
RomanDonw <romandonw628@gmail.com>
2026-09-02 22:40:22 +1000

    started development of the project (created the base of the project)

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/*
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+#include <dspmodule.h>
+
+const unsigned short dspmodule_requiredAPIversion = 1;
+
+unsigned short dspmodule_startup(const DSPLoaderAPI *lapi, int argc, char * const argv[], const char **sysname, const char **dispname)
+{
+    return 1;
+}
+
+unsigned short dspmodule_process(const DSPLoaderAPI *lapi, unsigned long long position, unsigned long duration, unsigned long rate, unsigned long long nsectime)
+{
+    return 1;
+}
+
+void dspmodule_cleanup(void) {}
