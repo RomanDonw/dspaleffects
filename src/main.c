3e8ce59e74821f8af29c5f53ea8cecc3f29ea70a
RomanDonw <romandonw628@gmail.com>
2026-09-03 13:50:22 +1000

    created the base of OpenAL loopback device initialization

--- src/main.c
+++ src/main.c
@@ -6,12 +6,43 @@
 
 #include <dspmodule.h>
 
+#include <stddef.h>
+#include <stdio.h>
+
 #include <al.h>
+#include <alc.h>
+#include <alext.h>
 
 const unsigned short dspmodule_requiredAPIversion = 1;
 
+static LPALCRENDERSAMPLESSOFT alcRenderSamplesSOFT;
+static ALCdevice *aldev;
+static ALCcontext *alctx;
+
 unsigned short dspmodule_startup(const DSPLoaderAPI *lapi, int argc, char * const argv[], const char **sysname, const char **dispname)
 {
+    if (!alcIsExtensionPresent(NULL, "ALC_SOFT_loopback"))
+    { puts("required ALC_SOFT_loopback OpenAL extension doesn't supported on this platform"); return 1; }
+
+    LPALCLOOPBACKOPENDEVICESOFT alcLoopbackOpenDeviceSOFT = alcGetProcAddress(NULL, "alcLoopbackOpenDeviceSOFT");
+    if (!alcLoopbackOpenDeviceSOFT) { puts("failed to dynamicly load alcLoopbackOpenDeviceSOFT OpenAL function"); return 1; }
+    if (!(alcRenderSamplesSOFT = alcGetProcAddress(NULL, "alcRenderSamplesSOFT")))
+    { puts("failed to dynamicly load alcRenderSamplesSOFT OpenAL function"); return 1; }
+    
+    if (!(aldev = alcLoopbackOpenDeviceSOFT(NULL))) { puts("error creating/opening OpenAL loopback device"); return 1; }
+
+    ALCint attrs[] =
+    {
+        ALC_FORMAT_CHANNELS_SOFT, ALC_STEREO_SOFT,
+        ALC_FORMAT_TYPE_SOFT, ALC_FLOAT_SOFT,
+        ALC_FREQUENCY, 48000,
+        0
+    };
+    if (!(alctx = alcCreateContext(aldev, attrs))) { puts("error creating OpenAL context for loopback device"); return 1; }
+    alcMakeContextCurrent(alctx);
+
+    
+
     return 1;
 }
 
@@ -20,4 +51,9 @@ unsigned short dspmodule_process(const DSPLoaderAPI *lapi, unsigned long long po
     return 1;
 }
 
-void dspmodule_cleanup(void) {}
+void dspmodule_cleanup(void)
+{
+    alcMakeContextCurrent(NULL);
+    alcDestroyContext(alctx);
+    alcCloseDevice(aldev);
+}
