b8cb8eafdb0b716cd004c9024c96d1247525d8d1
RomanDonw <romandonw628@gmail.com>
2026-09-25 18:06:43 +1000

    added command line parameter -s to disable strong requiring all config options to both current completed/available effects (EAX. reverb & chorus)

--- src/chorus.c
+++ src/chorus.c
@@ -25,14 +25,17 @@ const unsigned short dspmodule_requiredAPIversion = 1;
 static float origgain = 1, effectgain = 1, inampmod = 0, involmod = 1, outampmod = 0, outvolmod = 1;
 static void *inleftport, *inrightport, *outleftport, *outrightport;
 static ALuint source, buffer;
-static bool allowidlerenders = false;
+static bool allowidlerenders = false, strongconfoptcheck = true;
 
 static void printeffectprops(ALuint effect);
 
 #define GETFLTCONFOPTHELPER(strname, alname) \
-    if (!json_object_object_get_ex(configroot, strname, &jobj)) { puts("key \"" strname "\" doesnt found in config file"); return 1; }\
-    if (jsonutil_getfloat(jobj, vec3f)) { puts("parsing \"" strname "\" config option failed (required float)"); return 1; }\
-    alEffectf(effect, alname, *vec3f);
+    if (json_object_object_get_ex(configroot, strname, &jobj))\
+    {\
+        if (jsonutil_getfloat(jobj, vec3f)) { puts("parsing \"" strname "\" config option failed (required float)"); return 1; }\
+        alEffectf(effect, alname, *vec3f);\
+    }\
+    else if (strongconfoptcheck) { puts("key \"" strname "\" doesnt found in config file"); return 1; }
 
 unsigned short dspmodule_startup(const DSPLoaderAPI *lapi, int argc, char * const argv[], const char **sysname, const char **dispname)
 {   
@@ -40,7 +43,7 @@ unsigned short dspmodule_startup(const DSPLoaderAPI *lapi, int argc, char * cons
     struct json_object *configroot = NULL;
     {
         int p;
-        while ((p = getopt(argc, argv, "g:G:f:a:v:A:V:i:")) != -1)
+        while ((p = getopt(argc, argv, "g:G:f:a:v:A:V:is")) != -1)
         {
             switch (p)
             {
@@ -103,6 +106,10 @@ unsigned short dspmodule_startup(const DSPLoaderAPI *lapi, int argc, char * cons
 
                 case 'i':
                     allowidlerenders = true;
+                    break;
+
+                case 's':
+                    strongconfoptcheck = false;
                     break;
             }
         }
