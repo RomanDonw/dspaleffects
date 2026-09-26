9aa29eb488f27c088e9fa0f5722aede19866660b
RomanDonw <romandonw628@gmail.com>
2026-09-26 14:18:04 +1000

    added possible passing effect settings through long command-line parameters (if set, overrides exist config/default values) to eaxreverb effect

--- src/eaxreverb.c
+++ src/eaxreverb.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include <json-c/json_object.h>
 #include <json-c/json_tokener.h>
@@ -28,37 +29,119 @@ static bool allowidlerenders = false, strongconfoptcheck = true;
 
 static void printeffectprops(ALuint effect);
 
-#define GETFLTVEC3CONFOPTHELPER(strname, alname) \
-    if (json_object_object_get_ex(configroot, strname, &jobj))\
-    {\
-        if (jsonutil_getvec3f(jobj, vec3f)) { puts("failed parsing option \"" strname "\" (required vector/array of 3 floats)"); return 1; }\
-        alEffectfv(effect, alname, vec3f);\
-    }\
-    else if (strongconfoptcheck) { puts("key \"" strname "\" doesnt found in config file"); return 1; }
+struct floatopt { bool has; float value; } typedef floatopt;
+struct intopt { bool has; int value; } typedef intopt;
+struct vec3opt { bool has; float value[3]; } typedef vec3opt;
 
-#define GETFLTCONFOPTHELPER(strname, alname) \
-    if (json_object_object_get_ex(configroot, strname, &jobj))\
+#define GETFLTVEC3CONFOPTHELPER(vec3optsidx, strname, alname) \
+    if (!vec3opts[vec3optsidx].has)\
     {\
-        if (jsonutil_getfloat(jobj, vec3f)) { puts("parsing \"" strname "\" config option failed (required float)"); return 1; }\
-        alEffectf(effect, alname, *vec3f);\
-    }\
-    else if (strongconfoptcheck) { puts("key \"" strname "\" doesnt found in config file"); return 1; }
+        if (json_object_object_get_ex(configroot, strname, &jobj))\
+        {\
+            if (jsonutil_getvec3f(jobj, vec3f)) { puts("failed parsing option \"" strname "\" (required vector/array of 3 floats)"); return 1; }\
+            alEffectfv(effect, alname, vec3f);\
+        }\
+        else if (strongconfoptcheck) { puts("key \"" strname "\" doesnt found in config file"); return 1; }\
+    }
 
-#define GETBOOLCONFOPTHELPER(strname, alname) \
-    if (json_object_object_get_ex(configroot, strname, &jobj))\
+#define GETFLTCONFOPTHELPER(floatoptsidx, strname, alname) \
+    if (!floatopts[floatoptsidx].has)\
     {\
-        if (jsonutil_getbool(jobj, &flag)) { puts("parsing \"" strname "\" config option failed (required boolean)"); return 1; }\
-        alEffecti(effect, alname, flag);\
-    }\
-    else if (strongconfoptcheck) { puts("key \"" strname "\" doesnt found in config file"); return 1; }
+        if (json_object_object_get_ex(configroot, strname, &jobj))\
+        {\
+            if (jsonutil_getfloat(jobj, vec3f)) { puts("parsing \"" strname "\" config option failed (required float)"); return 1; }\
+            alEffectf(effect, alname, *vec3f);\
+        }\
+        else if (strongconfoptcheck) { puts("key \"" strname "\" doesnt found in config file"); return 1; }\
+    }
 
+#define GETBOOLCONFOPTHELPER(intoptsidx, strname, alname) \
+    if (!intopts[intoptsidx].has)\
+    {\
+        if (json_object_object_get_ex(configroot, strname, &jobj))\
+        {\
+            if (jsonutil_getbool(jobj, &flag)) { puts("parsing \"" strname "\" config option failed (required boolean)"); return 1; }\
+            alEffecti(effect, alname, flag);\
+        }\
+        else if (strongconfoptcheck) { puts("key \"" strname "\" doesnt found in config file"); return 1; }\
+    }
+
+#define PARSELONGFLOATOPT(optid, optindex, optname) \
+    case optid:\
+        if (sscanf(optarg, "%f", &floatopts[optindex].value) < 1) { puts("error parsing option " optname " (required float)"); return 1; }\
+        floatopts[optindex].has = true;\
+        break;
+
+#define PARSELONGVEC3OPT(optid, optindex, optname) \
+    case optid:\
+        if (sscanf(optarg, "%f,%f,%f", &vec3opts[optindex].value[0], &vec3opts[optindex].value[1], &vec3opts[optindex].value[2]) < 3)\
+        { puts("error parsing option " optname " (required 3D vector - \"float, float, float\")"); return 1; }\
+        vec3opts[optindex].has = true;\
+        break;
+
+#define PARSELONGBOOLOPT(optid, optindex, optname) \
+    case optid:\
+        if (sscanf(optarg, "%i", &intopts[optindex].value) >= 1);\
+        else if (!strcmp(optarg, "true") || !strcmp(optarg, "on") || !strcmp(optarg, "enable") || !strcmp(optarg, "yes"))\
+            intopts[optindex].value = true;\
+        else if (!strcmp(optarg, "false") || !strcmp(optarg, "off") || !strcmp(optarg, "disable") || !strcmp(optarg, "no"))\
+            intopts[optindex].value = false;\
+        else\
+        {\
+            puts("error parsing option " optname " (required boolean, allowed values: integer (where 0 - disable, other - enable), "\
+                "\"true\"/\"on\"/\"enable\"/\"yes\" or \"false\"/\"off\"/\"disable\"/no\")");\
+            return 1;\
+        }\
+        intopts[optindex].has = true;\
+        break;
+
+#define SETEFFFLOATPROPFROMOPT(optindex, alname) \
+    if (floatopts[optindex].has) alEffectf(effect, alname, floatopts[optindex].value);
+#define SETEFFVEC3PROPFROMOPT(optindex, alname) \
+    if (vec3opts[optindex].has) alEffectfv(effect, alname, vec3opts[optindex].value);
+#define SETEFFINTPROPFROMOPT(optindex, alname) \
+    if (intopts[optindex].has) alEffecti(effect, alname, intopts[optindex].value);
+    
 unsigned short dspmodule_startup(const DSPLoaderAPI *lapi, int argc, char * const argv[], const char **sysname, const char **dispname)
 {   
     const char *configfilename = NULL;
     struct json_object *configroot = NULL;
+
+    floatopt floatopts[20] = {0};
+    intopt intopts[1] = {0};
+    vec3opt vec3opts[2] = {0};
+
     {
         int p;
-        while ((p = getopt(argc, argv, "g:G:f:a:v:A:V:is")) != -1)
+        static const struct option longopts[] =
+        {
+            { .name = "density", .has_arg = required_argument, .val = 256, .flag = NULL },
+            { .name = "diffusion", .has_arg = required_argument, .val = 257, .flag = NULL },
+            { .name = "gain", .has_arg = required_argument, .val = 258, .flag = NULL },
+            { .name = "gainHF", .has_arg = required_argument, .val = 259, .flag = NULL },
+            { .name = "gainLF", .has_arg = required_argument, .val = 260, .flag = NULL },
+            { .name = "decayTime", .has_arg = required_argument, .val = 261, .flag = NULL },
+            { .name = "decayHFRatio", .has_arg = required_argument, .val = 262, .flag = NULL },
+            { .name = "decayLFRatio", .has_arg = required_argument, .val = 263, .flag = NULL },
+            { .name = "reflectionsGain", .has_arg = required_argument, .val = 264, .flag = NULL },
+            { .name = "reflectionsDelay", .has_arg = required_argument, .val = 265, .flag = NULL },
+            { .name = "lateReverbGain", .has_arg = required_argument, .val = 266, .flag = NULL },
+            { .name = "lateReverbDelay", .has_arg = required_argument, .val = 267, .flag = NULL },
+            { .name = "echoTime", .has_arg = required_argument, .val = 268, .flag = NULL },
+            { .name = "echoDepth", .has_arg = required_argument, .val = 269, .flag = NULL },
+            { .name = "modulationTime", .has_arg = required_argument, .val = 270, .flag = NULL },
+            { .name = "modulationDepth", .has_arg = required_argument, .val = 271, .flag = NULL },
+            { .name = "airAbsorptionGainHF", .has_arg = required_argument, .val = 272, .flag = NULL },
+            { .name = "HFReference", .has_arg = required_argument, .val = 273, .flag = NULL },
+            { .name = "LFReference", .has_arg = required_argument, .val = 274, .flag = NULL },
+            { .name = "roomRolloffFactor", .has_arg = required_argument, .val = 275, .flag = NULL },
+            
+            { .name = "decayHFLimit", .has_arg = required_argument, .val = 276, .flag = NULL },
+            
+            { .name = "reflectionsPan", .has_arg = required_argument, .val = 277, .flag = NULL },
+            { .name = "lateReverbPan", .has_arg = required_argument, .val = 278, .flag = NULL },
+        };
+        while ((p = getopt_long(argc, argv, "g:G:f:a:v:A:V:is", longopts, NULL)) != -1)
         {
             switch (p)
             {
@@ -126,6 +209,38 @@ unsigned short dspmodule_startup(const DSPLoaderAPI *lapi, int argc, char * cons
                 case 's':
                     strongconfoptcheck = false;
                     break;
+
+                /*
+                    ==========================
+                    ====== LONG OPTIONS ======
+                    ===========================
+                */
+
+                PARSELONGFLOATOPT(256, 0, "--density")
+                PARSELONGFLOATOPT(257, 1, "--diffusion")
+                PARSELONGFLOATOPT(258, 2, "--gain")
+                PARSELONGFLOATOPT(259, 3, "--gainHF")
+                PARSELONGFLOATOPT(260, 4, "--gainLF")
+                PARSELONGFLOATOPT(261, 5, "--decayTime")
+                PARSELONGFLOATOPT(262, 6, "--decayHFRatio")
+                PARSELONGFLOATOPT(263, 7, "--decayLFRatio")
+                PARSELONGFLOATOPT(264, 8, "--reflectionsGain")
+                PARSELONGFLOATOPT(265, 9, "--reflectionsDelay")
+                PARSELONGFLOATOPT(266, 10, "--lateReverbGain")
+                PARSELONGFLOATOPT(267, 11, "--lateReverbDelay")
+                PARSELONGFLOATOPT(268, 12, "--echoTime")
+                PARSELONGFLOATOPT(269, 13, "--echoDepth")
+                PARSELONGFLOATOPT(270, 14, "--modulationTime")
+                PARSELONGFLOATOPT(271, 15, "--modulationDepth")
+                PARSELONGFLOATOPT(272, 16, "--airAbsorptionGainHF")
+                PARSELONGFLOATOPT(273, 17, "--HFReference")
+                PARSELONGFLOATOPT(274, 18, "--LFReference")
+                PARSELONGFLOATOPT(275, 19, "--roomRolloffFactor")
+
+                PARSELONGBOOLOPT(276, 0, "--decayHFLimit")
+                
+                PARSELONGVEC3OPT(277, 0, "--reflectionsPan")
+                PARSELONGVEC3OPT(278, 1, "--lateReverbPan")
             }
         }
     }
@@ -145,33 +260,58 @@ unsigned short dspmodule_startup(const DSPLoaderAPI *lapi, int argc, char * cons
         float vec3f[3];
         struct json_object *jobj;
 
-        GETFLTCONFOPTHELPER("density", AL_EAXREVERB_DENSITY);
-        GETFLTCONFOPTHELPER("diffusion", AL_EAXREVERB_DIFFUSION);
-        GETFLTCONFOPTHELPER("gain", AL_EAXREVERB_GAIN);
-        GETFLTCONFOPTHELPER("gainHF", AL_EAXREVERB_GAINHF);
-        GETFLTCONFOPTHELPER("gainLF", AL_EAXREVERB_GAINLF);
-        GETFLTCONFOPTHELPER("decayTime", AL_EAXREVERB_DECAY_TIME);
-        GETFLTCONFOPTHELPER("decayHFRatio", AL_EAXREVERB_DECAY_HFRATIO);
-        GETFLTCONFOPTHELPER("decayLFRatio", AL_EAXREVERB_DECAY_LFRATIO);
-        GETFLTCONFOPTHELPER("reflectionsDelay", AL_EAXREVERB_REFLECTIONS_DELAY);
-        GETFLTCONFOPTHELPER("reflectionsGain", AL_EAXREVERB_REFLECTIONS_GAIN);
-        GETFLTVEC3CONFOPTHELPER("reflectionsPan", AL_EAXREVERB_REFLECTIONS_PAN);
-        GETFLTCONFOPTHELPER("lateReverbDelay", AL_EAXREVERB_LATE_REVERB_DELAY);
-        GETFLTCONFOPTHELPER("lateReverbGain", AL_EAXREVERB_LATE_REVERB_GAIN);
-        GETFLTVEC3CONFOPTHELPER("lateReverbPan", AL_EAXREVERB_LATE_REVERB_PAN);
-        GETFLTCONFOPTHELPER("lateReverbDelay", AL_EAXREVERB_LATE_REVERB_DELAY);
-        GETFLTCONFOPTHELPER("echoDepth", AL_EAXREVERB_ECHO_DEPTH);
-        GETFLTCONFOPTHELPER("echoTime", AL_EAXREVERB_ECHO_TIME);
-        GETFLTCONFOPTHELPER("modulationDepth", AL_EAXREVERB_MODULATION_DEPTH);
-        GETFLTCONFOPTHELPER("modulationTime", AL_EAXREVERB_MODULATION_TIME);
-        GETFLTCONFOPTHELPER("airAbsorptionGainHF", AL_EAXREVERB_AIR_ABSORPTION_GAINHF);
-        GETFLTCONFOPTHELPER("HFReference", AL_EAXREVERB_HFREFERENCE);
-        GETFLTCONFOPTHELPER("LFReference", AL_EAXREVERB_LFREFERENCE);
-        GETFLTCONFOPTHELPER("roomRolloffFactor", AL_EAXREVERB_ROOM_ROLLOFF_FACTOR);
-        GETBOOLCONFOPTHELPER("decayHFLimit", AL_EAXREVERB_DECAY_HFLIMIT);
+        GETFLTCONFOPTHELPER(0, "density", AL_EAXREVERB_DENSITY);
+        GETFLTCONFOPTHELPER(1, "diffusion", AL_EAXREVERB_DIFFUSION);
+        GETFLTCONFOPTHELPER(2, "gain", AL_EAXREVERB_GAIN);
+        GETFLTCONFOPTHELPER(3, "gainHF", AL_EAXREVERB_GAINHF);
+        GETFLTCONFOPTHELPER(4, "gainLF", AL_EAXREVERB_GAINLF);
+        GETFLTCONFOPTHELPER(5, "decayTime", AL_EAXREVERB_DECAY_TIME);
+        GETFLTCONFOPTHELPER(6, "decayHFRatio", AL_EAXREVERB_DECAY_HFRATIO);
+        GETFLTCONFOPTHELPER(7, "decayLFRatio", AL_EAXREVERB_DECAY_LFRATIO);
+        GETFLTCONFOPTHELPER(8, "reflectionsGain", AL_EAXREVERB_REFLECTIONS_GAIN);
+        GETFLTCONFOPTHELPER(9, "reflectionsDelay", AL_EAXREVERB_REFLECTIONS_DELAY);
+        GETFLTVEC3CONFOPTHELPER(0, "reflectionsPan", AL_EAXREVERB_REFLECTIONS_PAN);
+        GETFLTCONFOPTHELPER(10, "lateReverbGain", AL_EAXREVERB_LATE_REVERB_GAIN);
+        GETFLTCONFOPTHELPER(11, "lateReverbDelay", AL_EAXREVERB_LATE_REVERB_DELAY);
+        GETFLTVEC3CONFOPTHELPER(1, "lateReverbPan", AL_EAXREVERB_LATE_REVERB_PAN);
+        GETFLTCONFOPTHELPER(12, "echoDepth", AL_EAXREVERB_ECHO_DEPTH);
+        GETFLTCONFOPTHELPER(13, "echoTime", AL_EAXREVERB_ECHO_TIME);
+        GETFLTCONFOPTHELPER(14, "modulationDepth", AL_EAXREVERB_MODULATION_DEPTH);
+        GETFLTCONFOPTHELPER(15, "modulationTime", AL_EAXREVERB_MODULATION_TIME);
+        GETFLTCONFOPTHELPER(16, "airAbsorptionGainHF", AL_EAXREVERB_AIR_ABSORPTION_GAINHF);
+        GETFLTCONFOPTHELPER(17, "HFReference", AL_EAXREVERB_HFREFERENCE);
+        GETFLTCONFOPTHELPER(18, "LFReference", AL_EAXREVERB_LFREFERENCE);
+        GETFLTCONFOPTHELPER(19, "roomRolloffFactor", AL_EAXREVERB_ROOM_ROLLOFF_FACTOR);
+        GETBOOLCONFOPTHELPER(0, "decayHFLimit", AL_EAXREVERB_DECAY_HFLIMIT);
 
         json_object_put(configroot);
     }
+    
+    SETEFFFLOATPROPFROMOPT(0, AL_EAXREVERB_DENSITY);
+    SETEFFFLOATPROPFROMOPT(1, AL_EAXREVERB_DIFFUSION);
+    SETEFFFLOATPROPFROMOPT(2, AL_EAXREVERB_GAIN);
+    SETEFFFLOATPROPFROMOPT(3, AL_EAXREVERB_GAINHF);
+    SETEFFFLOATPROPFROMOPT(4, AL_EAXREVERB_GAINLF);
+    SETEFFFLOATPROPFROMOPT(5, AL_EAXREVERB_DECAY_TIME);
+    SETEFFFLOATPROPFROMOPT(6, AL_EAXREVERB_DECAY_HFRATIO);
+    SETEFFFLOATPROPFROMOPT(7, AL_EAXREVERB_DECAY_LFRATIO);
+    SETEFFFLOATPROPFROMOPT(8, AL_EAXREVERB_REFLECTIONS_GAIN);
+    SETEFFFLOATPROPFROMOPT(9, AL_EAXREVERB_REFLECTIONS_DELAY);
+    SETEFFFLOATPROPFROMOPT(10, AL_EAXREVERB_LATE_REVERB_GAIN);
+    SETEFFFLOATPROPFROMOPT(11, AL_EAXREVERB_LATE_REVERB_DELAY);
+    SETEFFFLOATPROPFROMOPT(12, AL_EAXREVERB_ECHO_TIME);
+    SETEFFFLOATPROPFROMOPT(13, AL_EAXREVERB_ECHO_DEPTH);
+    SETEFFFLOATPROPFROMOPT(14, AL_EAXREVERB_MODULATION_TIME);
+    SETEFFFLOATPROPFROMOPT(15, AL_EAXREVERB_MODULATION_DEPTH);
+    SETEFFFLOATPROPFROMOPT(16, AL_EAXREVERB_AIR_ABSORPTION_GAINHF);
+    SETEFFFLOATPROPFROMOPT(17, AL_EAXREVERB_HFREFERENCE);
+    SETEFFFLOATPROPFROMOPT(18, AL_EAXREVERB_LFREFERENCE);
+    SETEFFFLOATPROPFROMOPT(19, AL_EAXREVERB_ROOM_ROLLOFF_FACTOR);
+
+    SETEFFINTPROPFROMOPT(0, AL_EAXREVERB_DECAY_HFLIMIT);
+
+    SETEFFVEC3PROPFROMOPT(0, AL_EAXREVERB_REFLECTIONS_PAN);
+    SETEFFVEC3PROPFROMOPT(1, AL_EAXREVERB_LATE_REVERB_PAN);
 
     // ===============================
 
